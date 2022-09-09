8a7de7bea341f14017b58a2703ba40302c111527
Coderbros1811 <113094405+Coderbros1811@users.noreply.github.com>
2022-09-09 12:47:36 +0530

    Update data.sql

--- springbootdemo/src/main/resources/data.sql
+++ springbootdemo/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into userdata values(101,'Tom');  
+insert into userdata values(101,'Tommy');  
 insert into userdata values(102,'Andrew');  
 insert into userdata values(103,'Tony');  
 insert into userdata values(104,'Bob');  
